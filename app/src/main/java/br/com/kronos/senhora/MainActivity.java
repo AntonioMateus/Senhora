5dff44a568e01ce58b829249f874ba68c24c1ff7
antonio <antoniocms1995@hotmail.com>
2015-10-21 09:45:37 -0200

    Senhora funcionando !

--- app/src/main/java/br/com/kronos/senhora/MainActivity.java
+++ app/src/main/java/br/com/kronos/senhora/MainActivity.java
@@ -2,14 +2,23 @@ package br.com.kronos.senhora;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -17,22 +26,26 @@ public class MainActivity extends Activity {
     protected EditText mEmail;
     protected EditText mPassword;
     protected Button registerButton;
+    protected Button botaoExibir;
+    ListView mainListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Log.d
         setContentView(R.layout.activity_main);
+        ArrayList<String> dadosListView = new ArrayList<String>();
         //inicializar
         mUsername = (EditText) findViewById(R.id.usernameRegisterEditText);
         mEmail = (EditText) findViewById(R.id.emailRegisterEditText);
         mPassword = (EditText) findViewById(R.id.passwordRegisterEditText);
         registerButton = (Button) findViewById(R.id.submit);
-
-        //listen to register button click
+        botaoExibir = (Button) findViewById(R.id.exibir);
+        final FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(MainActivity.this);
+          //listen to register button click
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(MainActivity.this);
                 SQLiteDatabase db = mDbHelper.getWritableDatabase();
                 ContentValues values = new ContentValues();
                 values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_NOME,mUsername.getText().toString().trim());
@@ -44,6 +57,28 @@ public class MainActivity extends Activity {
                 Toast.makeText(MainActivity.this,"Inscrição concluida",Toast.LENGTH_LONG).show();
             }
         });
+        botaoExibir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = mDbHelper.getReadableDatabase();
+                String[] projection = {FeedReaderContract.FeedEntry.COLUMN_NAME_NOME};
+                Cursor c = db.query(FeedReaderContract.FeedEntry.TABLE_NAME,projection,null,null,null,null,null);
+                ArrayAdapter<String> listAdapter;
+                ArrayList<String> nomes = new ArrayList<>();
+                if (c.getCount()>0) {
+                    c.moveToFirst();
+                    while (!c.isAfterLast()) {
+                        nomes.add(c.getString(0));
+                        c.moveToNext();
+                    }
+                }
+                mainListView = (ListView) findViewById(R.id.listView);
+                listAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, nomes);
+                mainListView.setAdapter(listAdapter);
+            }
+        });
+
+
 
     }
 
